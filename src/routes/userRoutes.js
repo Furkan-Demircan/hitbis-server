3c37b6244619dd366ed8b7a59cb2ba6b3cae9391
computeneer <computeneer@gmail.com>
2025-02-03 22:59:59 +0300

    new user routes added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
-import { createUserValidation } from "../validations/userValidations.js";
+import { createUserValidation, editUserValidation, resetPasswordValidation } from "../validations/userValidations.js";
 import { validator } from "../middlewares/validator.js";
+import authenticateMiddleware from "../middlewares/authenticationMiddleware.js";
 
 const userRoutes = Router();
 
 userRoutes.post("/register", validator(createUserValidation), userController.createUser);
+userRoutes.get("/profile", authenticateMiddleware, userController.getProfileByToken);
+userRoutes.post("/edit", validator(editUserValidation), authenticateMiddleware, userController.editUser);
+userRoutes.post(
+	"/resetpassword",
+	validator(resetPasswordValidation),
+	authenticateMiddleware,
+	userController.resetPassword
+);
 
 export default userRoutes;

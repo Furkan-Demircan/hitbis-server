59b578b98ca6ef361ac9e71244ccfaf3fd830d7e
Furkan <goooglenudle@gmail.com>
2025-02-24 22:11:47 +0300

    add communityItemDto class for user-community relationships

--- src/dto/communityDtos.js
+++ src/dto/communityDtos.js
@@ -1,16 +1,29 @@
 export class CommunityInfoDto {
-	id;
-	name;
-	description;
-	isPublic;
-	country;
-	city;
-	constructor(id, name, description, isPublic, country, city) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.isPublic = isPublic;
-		this.country = country;
-		this.city = city;
-	}
+  id;
+  name;
+  description;
+  isPublic;
+  country;
+  city;
+  constructor(id, name, description, isPublic, country, city) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.isPublic = isPublic;
+    this.country = country;
+    this.city = city;
+  }
+}
+
+export class communityItemDto {
+  userId;
+  communityId;
+  isAdmin;
+  isBanned;
+  constructor(userId, communityId, isAdmin, isBanned) {
+    this.userId = userId;
+    this.communityId = communityId;
+    this.isAdmin = isAdmin;
+    this.isBanned = isBanned;
+  }
 }

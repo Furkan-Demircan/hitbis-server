48949a24d19b9a3980b2ee90a2e1532f6280b16e
Furkan Demircan <goooglenudle@gmail.com>
2025-05-01 22:44:30 +0300

    fix(user): correct indentation and formatting in User model schema

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,53 +1,58 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
-	{
-		username: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-		surname: { type: String, required: true },
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		birthDate: {
-			type: Date,
-		},
-		weight: {
-			type: Number,
-		},
-		height: {
-			type: Number,
-		},
-		isSignUpComplete: {
-			type: Boolean,
-			default: false,
-		},
-		isBanned: {
-			type: Boolean,
-			default: false,
-		},
-		isActive: {
-			type: Boolean,
-			default: false,
-		},
-		isDeleted: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	{ timestamps: true, collection: "users" }
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        surname: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        birthDate: {
+            type: Date,
+        },
+        weight: {
+            type: Number,
+        },
+        height: {
+            type: Number,
+        },
+        isSignUpComplete: {
+            type: Boolean,
+            default: false,
+        },
+        isBanned: {
+            type: Boolean,
+            default: false,
+        },
+        isActive: {
+            type: Boolean,
+            default: false,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        role: {
+            type: String,
+            enum: ["admin", "user"],
+            default: "user",
+        },
+    },
+    { timestamps: true, collection: "users" }
 );
 
 export default mongoose.model("User", userSchema);
